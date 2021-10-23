df813f9fb17ba478fe8233f6d4b3cf6e2dfa45aa
Victor Alvarenga <info@victoralvarenga.com>
2021-10-23 11:07:15 -0300

    Feature: Create contribution IO

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -8,6 +8,9 @@ import UsersRepository from '@modules/users/repositories/typeOrm/UsersRepository
 import { IDreamsRepository } from '@modules/dreams/repositories/IDreamsRepository';
 import { DreamsRepository } from '@modules/dreams/repositories/typeOrm/DreamsRepository';
 
+import { IContributionsRepository } from '@modules/contributions/repositories/IContributionsRepository';
+import { ContributionsRepository } from '@modules/contributions/repositories/typeOrm/ContributionsRepository';
+
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
@@ -16,4 +19,9 @@ container.registerSingleton<IUsersRepository>(
 container.registerSingleton<IDreamsRepository>(
   'DreamsRepository',
   DreamsRepository,
+);
+
+container.registerSingleton<IContributionsRepository>(
+  'ContributionsRepository',
+  ContributionsRepository,
 );
--- src/shared/infra/http/routes.ts
+++ src/shared/infra/http/routes.ts
@@ -3,11 +3,13 @@ import { Router } from 'express';
 import usersRouter from '@modules/users/http/routes/users.routes';
 import sessionsRouter from '@modules/users/http/routes/sessions.routes';
 import { dreamsRouter } from '@modules/dreams/http/routes/dreams.routes';
+import { contributionsRouter } from '@modules/contributions/http/routes/contributions.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/dreams', dreamsRouter);
+routes.use('/contributions', contributionsRouter);
 
 export { routes };
