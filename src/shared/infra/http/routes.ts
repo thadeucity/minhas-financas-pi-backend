4c24c2ea8e3bb092a9559e13adc933d93717055f
Victor Alvarenga <info@victoralvarenga.com>
2021-10-23 09:26:47 -0300

    Feature: Add create dream IO

--- src/shared/infra/http/routes.ts
+++ src/shared/infra/http/routes.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import usersRouter from '@modules/users/http/routes/users.routes';
 import sessionsRouter from '@modules/users/http/routes/sessions.routes';
+import { dreamsRouter } from '@modules/dreams/http/routes/dreams.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
+routes.use('/dreams', dreamsRouter);
 
 export { routes };
